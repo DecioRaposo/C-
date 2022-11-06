3f6cf7e2e82888a882f39a54c3be8dd0e088fdc0
D.Raposo <decioraposopro@gmil.com>
2022-11-06 12:03:43 -0300

    ADD:ObjectMovUpDown

---
+++ MovUpDown.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovUpDown : MonoBehaviour
+{
+//++++++++++++++++++++++++++++++++
+//+++CONTRL MOV HIGHER or LOWER+++
+//++++++++++++++++++++++++++++++++
+//private float amp;
+//++++++++++++++++++++++++
+//+++CRONTROL MOV SPEED+++
+//++++++++++++++++++++++++
+//public float freq;
+    Vector3 initPos;
+
+    private void Start()
+    {
+        init.Pos = trnasform.position;
+    }
+    private void Update()
+    {
+        transform.position = new Vector3(initPos.x, Mathf.Sin(Time.time) /* * freq*/) /* amp*/ + initPos.y, 0);
+    }
+}
